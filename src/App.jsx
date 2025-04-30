1f3a940521368d009f8053b22f8b7cc00abc2a24
jjay <jayza2248@gmail.com>
2025-04-30 18:01:53 +0700

    Description of the changes you made5

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {  HashRouter as Router, Routes, Route } from "react-router-dom";
 import TopicL from "./components/TopicL";
 import MemberInput from "./components/MemberInput";
 import Login from "./components/Login";
